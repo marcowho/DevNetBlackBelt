347643280f1833d055ec5dbe2262eb05e8b07628
“marco” <marcowho@gmail.com>
2018-03-23 23:20:17 +1300

    poc folder removed

--- PoC-ACI/credentials.py
+++
@@ -1,6 +0,0 @@
-LOGIN    = "mhapi"
-PASSWORD = "aciapi1234"
-URL      = "https://172.16.10.10"
-MYSQLIP = '127.0.0.1'
-MYSQLLOGIN = 'root'
-MYSQLPASSWORD = 'redhat1234'
--- PoC-CMX/config.py
+++
@@ -1,7 +0,0 @@
-CMX_URL = 'https://cmxlocationsandbox.cisco.com'
-CMX_USER = 'learning'
-CMX_PASSW = 'learning'
-ACCESS_TOKEN    = "ZmVjMGY5ZmYtZmVhMi00MTc5LWJmYjgtMTNiOTc1MmFhZjdiMWEyNGViNzQtYmJl"
-MYSQLIP = '127.0.0.1'
-MYSQLUSER = 'root'
-MYSQLPASSWORD = 'redhat1234'
